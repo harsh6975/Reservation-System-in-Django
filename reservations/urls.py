a1ab23086e48847148148d6146bf4dc08481b6ce
Harsh Sinha <58680300+harsh6975@users.noreply.github.com>
2024-09-14 19:21:11 +0530

    urls and views for home page

--- reservations/urls.py
+++ reservations/urls.py
@@ -16,8 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from reservations import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('reservations.urls'))
+    path('',views.index,name='home'),
+   
 ]
